7fea21d3c152b656fd63290354749ab2fab7d589
Aryan-Naveen <aryan_naveen@college.harvard.edu>
2022-10-19 16:06:45 -0400

    test

--- src/upload_arduino.bash
+++ src/upload_arduino.bash
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-arduino-cli compile --fqbn arduino:avr:uno arduino_capturerefs/
-arduino-cli upload --port /dev/ttyACM0 arduino_capturerefs/
+arduino-cli compile --fqbn arduino:avr:uno arduino_doorctrl/
+arduino-cli upload --port /dev/ttyACM0 arduino_doorctrl/
